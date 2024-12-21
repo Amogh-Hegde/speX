c040d27402dce2da9c2d7bdb7e2f8c64e5809909
Harshendra M <151964309+harshendram@users.noreply.github.com>
2024-12-22 00:45:03 +0530

    Update app.py
    face recognition dry running model 1

--- app.py
+++ app.py
@@ -5,6 +5,40 @@ from datetime import datetime, date
 from pyttsx3 import init
 from num2words import num2words
 
+import cv2
+import face_recognition
+import os
+import numpy as np
+
+path = "C:\\Users\\Harsh\\OneDrive\\Desktop\\images"
+images = []
+classNames = [os.path.splitext(cl)[0] for cl in os.listdir(path)]
+for cl in os.listdir(path):
+    curimg = cv2.imread(f'{path}/{cl}')
+    images.append(curimg)
+
+encodeListKnown = [face_recognition.face_encodings(cv2.cvtColor(img, cv2.COLOR_BGR2RGB))[0] for img in images]
+
+def identify_person(engine):
+    cap = cv2.VideoCapture(0)
+    success, img = cap.read()
+    cap.release()
+    imgS = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
+    facesCurFrame = face_recognition.face_locations(imgS)
+    encodesCurFrame = face_recognition.face_encodings(imgS, facesCurFrame)
+    if facesCurFrame:
+        for encodeFace, faceLoc in zip(encodesCurFrame, facesCurFrame):
+            matches = face_recognition.compare_faces(encodeListKnown, encodeFace)
+            faceDis = face_recognition.face_distance(encodeListKnown, encodeFace)
+            matchIndex = np.argmin(faceDis)
+            name = classNames[matchIndex].upper() if matches[matchIndex] else "Unknown"
+            engine.say(f"{name} is in front of you.")
+            engine.runAndWait()
+    else:
+        engine.say("No person detected.")
+        engine.runAndWait()
+
+
 # Text-to-speech engine initialization
 engine = init()
 engine.setProperty('rate', 150)
